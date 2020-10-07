a172a7bdd09acf9072cd74fc2bced5512a1118c6
Woyu Wang <ccradical2468@gmail.com>
2020-10-07 13:28:15 -0700

    Prototypes are now inherited from Data

--- Assets/Scripts/Prototypes/Prototype.cs
+++ Assets/Scripts/Prototypes/Prototype.cs
@@ -1,18 +1,7 @@
 using System;
 using UnityEngine;
 
-public class Prototype : IIdentifiable, IPrototype {
-
-    [SerializeField] private string identifier;
-
-    public string GetIdentifier () {
-        return identifier;
-    }
-
-    public string InitializeIdentifier () {
-        identifier = new Guid ().ToString ();
-        return identifier;
-    }
+public class Prototype : Data, IIdentifiable, IPrototype {
 
     public virtual Instance GetInstance () {
         return new Instance ();
